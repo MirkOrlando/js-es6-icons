82e5fd5828b670c1c8bcbdd4b8e65aa4e53bdd78
MirkOrlando <mirko@orlando-m.it>
2022-04-11 15:23:54 +0200

    created jsnack 3

--- JSnack3/main.js
+++ JSnack3/main.js
@@ -9,5 +9,22 @@ Es:
   { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
 ]
 Crea un nuovo array con la lista dei mammiferi.
+*/
 
- */
+const animals = [
+  { nome: "leone", famiglia: "felidi", classe: "mammiferi" },
+  { nome: "cane", famiglia: "canidi", classe: "mammiferi" },
+  { nome: "gallina", famiglia: "fasianidi", classe: "uccelli" },
+  { nome: "pinguino", famiglia: "Spheniscidae", classe: "uccelli" },
+  { nome: "lucertola", famiglia: "lacertilia", classe: "rettili" },
+  { nome: "lemure", famiglia: "primati", classe: "mammiferi" },
+  { nome: "umano", famiglia: "ominidi", classe: "mammiferi" },
+  { nome: "canguro", famiglia: "marsupiali ", classe: "mammiferi" },
+  { nome: "rana", famiglia: "anura", classe: "anfibi" },
+];
+
+const mammals = animals.filter((animal) => {
+  return animal.classe === "mammiferi";
+});
+
+console.log(mammals);
